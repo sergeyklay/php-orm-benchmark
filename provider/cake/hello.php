98d320267082552d20df32b7c7c5ac88dbc3d5ad
Hari KT <kthari85@gmail.com>
2018-01-09 14:29:20 +0530

    Remove test file

--- provider/cake/hello.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-
-use OrmBench\Cake\Models\PostsTable;
-use Cake\ORM\TableRegistry;
-
-$postsTable = TableRegistry::get('Posts', [
-    'className' => PostsTable::class,
-]);
